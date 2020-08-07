95f89c23bb958c211c68379c868162cd5bdc5da9
ali-m-d <alistair_divorty@hotmail.com>
2020-08-07 22:31:06 +0000

    Updated config/environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.hosts << "6d199b1424e34b75afc7e772f48a1b9c.vfs.cloud9.eu-west-2.amazonaws.com"
+  config.web_console.whitelisted_ips = '86.189.229.227'
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
