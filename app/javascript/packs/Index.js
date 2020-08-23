a9be93a265e77fe48a78a859efc2f18c013223aa
ali-m-d <alistair_divorty@hotmail.com>
2020-08-23 15:34:49 +0000

    Imported gsap in Index.js

--- app/javascript/packs/Index.js
+++ app/javascript/packs/Index.js
@@ -9,6 +9,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import gsap from 'gsap';
 import App from '../components/App';
 import PropTypes from 'prop-types'
 
